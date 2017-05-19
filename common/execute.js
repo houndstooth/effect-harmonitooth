b389ec1fd2606a64de8394d06bd78592e8cfffdb
Douglas Blumeyer <Douglas Blumeyer>
2017-05-19 21:58:29 +0900

    remove console log

--- common/execute.js
+++ common/execute.js
@@ -6,7 +6,6 @@ export default () => {
 	let stripeCount = 1
 	setInterval(() => {
 		stripeCount *= STRIPE_COUNT_INCREASE_RATE
-		console.log(stripeCount)
 		harmonitoothFrame({ stripeCount })
 	}, FRAME_RATE)
 }
