55d8a656e212a7b8ee635d4300f4cb5243e557ec
Douglas Blumeyer <dblumeyer@pivotal.io>
2017-06-02 16:42:50 +0900

    collapse shared node of state to the top
    Signed-off-by: Douglas Blumeyer <dblumeyer@pivotal.io>

--- harmonitoothPreset.js
+++ harmonitoothPreset.js
@@ -1,18 +1,14 @@
 export default {
 	state: {
-		shared: {
-			stripeCountConfig: {
-				stripeCount: 1
-			},
-			stripeStyle: 'FULL_HARMONIC_CONTINUUM_COMPRESSED_INTO_SINGLE_TILE'
-		}
+		stripeCountConfig: {
+			stripeCount: 1
+		},
+		stripeStyle: 'FULL_HARMONIC_CONTINUUM_COMPRESSED_INTO_SINGLE_TILE'
 	},
 	iterations: {},
 	animations: {
-		shared: {
-			stripeCountConfig: {
-				stripeCount: p => p * 1.005
-			}
+		stripeCountConfig: {
+			stripeCount: p => p * 1.005
 		}
 	}
 }
