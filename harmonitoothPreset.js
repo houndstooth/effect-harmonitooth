ba90b5230fad74c5a009e49cdefeab2282776688
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2017-06-03 14:47:13 +0900

    no need for empty preset objects anymore

--- harmonitoothPreset.js
+++ harmonitoothPreset.js
@@ -7,7 +7,6 @@ export default {
 		},
 		stripeStyle: 'FULL_HARMONIC_CONTINUUM_COMPRESSED_INTO_SINGLE_TILE'
 	},
-	iterations: {},
 	animations: {
 		stripeCountConfig: {
 			stripeCount: p => p * ANIMATION_RATE
