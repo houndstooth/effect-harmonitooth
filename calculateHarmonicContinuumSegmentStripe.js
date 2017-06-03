30652fe52d14406a873aa492b84b89cec1c41bd0
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2017-06-03 12:53:51 +0900

    extract triangular numbers into a new shared math utilities

--- calculateHarmonicContinuumSegmentStripe.js
+++ calculateHarmonicContinuumSegmentStripe.js
@@ -1,7 +1,7 @@
-import triangularNumbers from './triangularNumbers'
+import mathUtilities from '../shared/utilities/mathUtilities'
 
 export default ({ stripeCount, stripeIndex }) => {
-	const { triangularNumber, triangularRoot } = triangularNumbers
+	const { triangularNumber, triangularRoot } = mathUtilities
 	const previous = stripeCount - 1
 	const tri = triangularNumber(previous)
 	const current = triangularRoot(tri + stripeIndex)
--- calculateHarmonicContinuumStripe.js
+++ calculateHarmonicContinuumStripe.js
@@ -1,6 +1,6 @@
-import triangularNumbers from './triangularNumbers'
+import mathUtilities from '../shared/utilities/mathUtilities'
 
 export default ({ stripeCount, stripeIndex }) => {
-	const { triangularRoot } = triangularNumbers
+	const { triangularRoot } = mathUtilities
 	return triangularRoot(stripeIndex) / triangularRoot(stripeCount)
 }
