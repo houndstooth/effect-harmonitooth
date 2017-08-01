a90ab78bcb6282cf73e652e0bb8aa8c2a887c8c2
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2017-08-02 00:43:39 +0800

    unit tests for harmonitooth

--- test/src/utilities/getHarmonicContinuumSegmentStripePositionsTest.js
+++ test/src/utilities/getHarmonicContinuumSegmentStripePositionsTest.js
@@ -1,5 +1,23 @@
-describe('placeholder13', () => {
-	it('is one', () => {
-		expect(true).toBe(true)
+import getHarmonicContinuumSegmentStripePositions from '../../../src/utilities/getHarmonicContinuumSegmentStripePositions'
+import store from '../../../../../store'
+import resetStore from '../../../../../test/helpers/resetStore'
+import composeMainHoundstooth from '../../../../../src/store/composeMainHoundstooth'
+
+describe('get harmonic continuum segment stripe positions', () => {
+	it('can calculate the same information that the gingham chevron continuum stripe positions can, just each tile individually figures itself out, inferring its position in the whole from its stripe count', () => {
+		resetStore(store)
+		composeMainHoundstooth({
+			houndstoothOverrides: {
+				basePattern: {
+					stripeSettings: {
+						stripePositionSettings: {
+							stripeCountSetting: 9,
+						},
+					},
+				},
+			},
+		})
+
+		expect(getHarmonicContinuumSegmentStripePositions()).toEqual([ 0, 0.23368793961408585, 0.4642491965729789, 0.6918060129541317, 0.9164728671689168, 1.1383571472170537, 1.3575597506858195, 1.5741756210067095, 1.7882942280559355 ])
 	})
 })
--- test/src/utilities/getHarmonicContinuumStripePositionsTest.js
+++ test/src/utilities/getHarmonicContinuumStripePositionsTest.js
@@ -1,5 +1,26 @@
-describe('placeholder14', () => {
-	it('is one', () => {
-		expect(true).toBe(true)
+import getHarmonicContinuumStripePositions from '../../../src/utilities/getHarmonicContinuumStripePositions'
+import store from '../../../../../store'
+import resetStore from '../../../../../test/helpers/resetStore'
+import composeMainHoundstooth from '../../../../../src/store/composeMainHoundstooth'
+
+describe('get harmonic continuum stripe positions', () => {
+	it('given a count of stripes in each striped tile, and the desired index, will tell you how far across that tile that stripe starts', () => {
+		resetStore(store)
+		composeMainHoundstooth({
+			houndstoothOverrides: {
+				basePattern: {
+					stripeSettings: {
+						stripePositionSettings: {
+							stripeCountSetting: 10,
+						},
+					},
+				},
+			},
+		})
+
+		expect(getHarmonicContinuumStripePositions()[ 0 ]).toBe(0)
+		expect(getHarmonicContinuumStripePositions()[ 1 ]).toBe(1 / 2)
+		expect(getHarmonicContinuumStripePositions()[ 3 ]).toBe(1)
+		expect(getHarmonicContinuumStripePositions()[ 6 ]).toBe(3 / 2)
 	})
 })
