383fdb08157ec3487b78a5d2e7a08ab6c98921e9
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2017-09-19 08:50:30 +0900

    use default exports in math utilities

--- src/components/getHarmonicContinuumSegmentStripePositions.js
+++ src/components/getHarmonicContinuumSegmentStripePositions.js
@@ -1,10 +1,9 @@
-import mathUtilities from '../../../../src/utilities/mathUtilities'
+import { triangularNumber, triangularRoot } from '../../../../src/utilities/mathUtilities'
 import src from '../../../../src'
 
 export default () => src.perStripe({ getStripePosition: harmonicContinuumSegmentStripePosition })
 
 const harmonicContinuumSegmentStripePosition = ({ stripeCount, stripeIndex }) => {
-	const { triangularNumber, triangularRoot } = mathUtilities
 	const previousStripePosition = stripeCount - 1
 	const previousStripePositionIndex = triangularNumber(previousStripePosition)
 	const currentStripePosition = triangularRoot(previousStripePositionIndex + stripeIndex)
--- src/components/getHarmonicContinuumStripePositions.js
+++ src/components/getHarmonicContinuumStripePositions.js
@@ -1,9 +1,6 @@
-import mathUtilities from '../../../../src/utilities/mathUtilities'
+import { triangularRoot } from '../../../../src/utilities/mathUtilities'
 import src from '../../../../src'
 
 export default () => src.perStripe({ getStripePosition: harmonicContinuumStripePosition })
 
-const harmonicContinuumStripePosition = ({ stripeIndex, stripeCount }) => {
-	const { triangularRoot } = mathUtilities
-	return triangularRoot(stripeIndex) / triangularRoot(stripeCount)
-}
+const harmonicContinuumStripePosition = ({ stripeIndex, stripeCount }) => triangularRoot(stripeIndex) / triangularRoot(stripeCount)
