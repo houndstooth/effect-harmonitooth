6207ba73fe0af75fe932f7cc6f55fd3d1af3a2e7
Douglas <douglas.blumeyer@gmail.com>
2017-10-23 02:52:40 +0800

    base address nominal typing on a singular (non-array) type

--- test/integration/helpers/expectWholeTile.ts
+++ test/integration/helpers/expectWholeTile.ts
@@ -1,14 +1,15 @@
-import { Address, Color, to } from '../../../../../src'
+import { Address, Color, from, to } from '../../../../../src'
 import { iterator } from '../../../../../src/utilities/codeUtilities'
 import { pixelIsColorWithMarker } from '../../../../../test/integration/helpers/pixelIsColorWithMarker'
 
 const expectWholeTile: (_: {
-	color: Color, gridAddress: Address,
+	color: Color, gridAddress: Address[],
 }) => void = ({ color: expectedColor, gridAddress }) => {
 	iterator(12).forEach(check => {
+		const [ x, y ] = from.Address(gridAddress)
 		const coordinateUnderTest = to.Coordinate([
-			(gridAddress[ 0 ] + check / 12) * 50,
-			(gridAddress[ 1 ] + check / 12) * 50,
+			(x + check / 12) * 50,
+			(y + check / 12) * 50,
 		])
 		expect(pixelIsColorWithMarker({ coordinateUnderTest, expectedColor, id: check })).toBe(true)
 	})
